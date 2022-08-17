134bc212e5c7ab279e793478edf82b09a1e3d3c8
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-08-17 00:22:22 -0400

    Added Production Settings

--- web_project/settings.py
+++ web_project/settings.py
@@ -36,6 +36,7 @@ NOTIFY_EMAIL = env('NOTIFY_EMAIL')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,6 +112,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if DEBUG is False:
     SESSION_COOKIE_SECURE = True
