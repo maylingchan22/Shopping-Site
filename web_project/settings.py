7204177be85d8a449b722d2e7ac1995ce1c05eb9
May Chan <91612351+maylingchan22@users.noreply.github.com>
2022-08-18 14:33:05 -0400

    Added Production settings

--- web_project/settings.py
+++ web_project/settings.py
@@ -109,7 +109,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
